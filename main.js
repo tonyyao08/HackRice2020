f8c481da23afb9c0cebf2eac587dc72757bf008a
Pearl Hwang <pearlh6527@gmail.com>
2020-09-19 12:16:18 -0400

    modified css and main.js + timeline.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ new Vue({
                 afterLoad: this.afterLoad,
                 navigation: true,
                 anchors: ['page1', 'page2', 'page3'],
-                sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#ee1a59', '#2c3e4f', '#ba5be9', '#b4b8ab']
+                sectionsColor: ['#41b883', '#FFFFFF', '#0798ec', '#fec401', '#1bcee6', '#ee1a59', '#2c3e4f', '#ba5be9', '#b4b8ab']
             },
         }
     },
@@ -58,3 +58,4 @@ new Vue({
         },
     }
 });
+
